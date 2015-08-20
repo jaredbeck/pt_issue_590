eae2e0ce0315fea790f5b4839be18f04ea23f85e
André Allavena <andre.allavena@gmail.com>
2015-08-20 12:53:48 +1000

    Simplified model. Works in console, fails in tests

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,15 @@
 class Document < ActiveRecord::Base
   has_many :sections
-  has_many :paragraphs, through: :sections
   has_paper_trail
+
+  def self.create_sample_document
+    d = Document.create!(title: "d v1")
+    s1 = Section.create!(document: d, title: "s1 v1")
+    s2 = Section.create!(document: d, title: "s2 v1")
+    puts 'Updating a section name'
+    s1.update_attributes(title: 's1 v2')
+    puts 'Updating document itself'
+    d.update_attributes(title: 'd v2')
+    d
+  end
 end
